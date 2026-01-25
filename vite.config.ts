d5c1f52b3d9e98de5db6e2e7d76f49c9dda11dbc
Ridhwan Ramdhani <ridhwan@ramdhani.me>
2026-01-25 17:52:27 +0100

    remove nitro

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,6 @@ export default defineConfig({
         }),
         tanstackStart(),
         viteReact(),
-        nitro({
-            preset: 'cloudflare-module',
-        }),
         cloudflare({ viteEnvironment: { name: 'ssr' } }),
     ],
 })
